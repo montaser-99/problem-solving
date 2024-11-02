5f8ebc9a381fd17a455d9e5244f7cda167663ad6
Mahmoud montaser <hicishicis112233@gmail.com>
2024-11-02 15:21:02 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// const arr1 = [1, 2, 4, 5, 8, 9, 10];
+
+// const evennumbers = function(arr) {
+//     return arr.filter((num) => num % 2 === 0);
+// };
+
+// console.log(evennumbers(arr1));
+// ////////////////////////////////////////////////////////////////////////////////
+
+// const arr1 = [1, 2, 4, 5, 8, 9, 22];
+// const largestNumber = function(arr) {
+//     return Math.max(...arr); // Spread the array into individual numbers
+// };
+
+// console.log(largestNumber(arr1)); 
+// //////////////////////////////////////////////////////////////////////////
+// const reverseString = function(str) {
+//     return str.split('').reverse().join('');
+// };
+
+
+// console.log(reverseString("hello")); 
+// ///////////////////////////////////////////////////////////////////////
+// const arr1=[1,2,3,4,5,5,6,,7,7]
+// const removeDuplicates = function(arr) {
+//     const uniqueArr = [];
+//     arr.forEach(num => {
+//         if (!uniqueArr.includes(num)) {
+//             uniqueArr.push(num);
+//         }
+//     });
+//     return uniqueArr;
+// };
+
+
+// console.log(removeDuplicates(arr1)); 
+// ////////////////////////////////////////////////////////////////////////
